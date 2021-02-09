f043948d508d21b83bb269a8b2ba211a5f3fd32e
Anna <nutkatuz@yandex.ru>
2021-02-09 23:04:27 +0500

    bugfix: constants in separate file

--- src/components/NewsCardList/NewsCardList.js
+++ src/components/NewsCardList/NewsCardList.js
@@ -4,15 +4,16 @@ import NewsCard from "../NewsCard/NewsCard";
 import Preloader from "../Preloader/Preloader";
 import noResultImage from "../../images/svg/no-result.svg";
 import "./NewsCardList.css";
+import { CARDSINROW } from "../../utils/constants";
 
 function NewsCardList({isSubmitted, onBtnClick, loggedIn, cards }) {
-    const { pathname } = useLocation();
+  
+  const { pathname } = useLocation();
 
-  //количество отрисованных карточек 3
+  //количество отрисованных карточек
 
   const [currentRow, setCurrentRow] = React.useState(0);
-
-  const trinityCards = cards.slice(0, (currentRow + 1) * 3);
+  const trinityCards = cards.slice(0, (currentRow + 1) * CARDSINROW);
   
   function handleShowMore() {
     setCurrentRow(currentRow + 1);
