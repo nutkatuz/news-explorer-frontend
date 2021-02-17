6e965e71d3ac095cbbd16677389d71b6d8a9c62b
Anna <nutkatuz@yandex.ru>
2021-02-17 19:00:04 +0500

    feat: preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,19 +1,15 @@
-import {React} from "react";
-import './Preloader.css'
+import { React } from "react";
+import "./Preloader.css";
 
-// Preloader — отвечает за работу прелоудера;
 function Preloader() {
-
-  // const [isLoading, setIsLoading] = useState(false);
-
   return (
     <>
-      <div className='container'>
-        <i className="circle-preloader"></i>
+      <div className="preloader">
+        <i className="preloader__circle"></i>
       </div>
-      <p  className="circle-preloader__text">Идет поиск новостей...</p>
+      <p className="preloader__text">Идет поиск новостей...</p>
     </>
-  )
+  );
 }
 
 export default Preloader;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
 export const ONEWEEK = 7 * 24 * 60 * 60 * 1000;
-export const CARDSINROW = 3;
+export const CARDSINROW = 6;
